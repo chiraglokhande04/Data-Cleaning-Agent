fa7829b20671a47b83e31894e8299cb61d10f354
chiraglokhande04 <chiraglokhande14@gmail.com>
2025-12-03 03:07:14 +0530

    change cors origin

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: "*",
+  origin: [
+  "http://localhost:5173",
+  "https://data-cleaning-agent.vercel.app"
+],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
