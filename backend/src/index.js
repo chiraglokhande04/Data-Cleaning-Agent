ed656c6a15b92ecb7bc3d271b360b80f57529778
chiraglokhande04 <chiraglokhande14@gmail.com>
2025-12-03 03:03:38 +0530

    change cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
